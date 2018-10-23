618aea3d82e9f71d105169ca2c9b1e126283b5ee
Spencercjh <shouspencercjh@foxmail.com>
2018-10-23 22:44:39 +0800

    解决具体Model Unused的问题；

--- app/src/main/java/top/spencer/crabscore/data/constant/Token.java
+++ app/src/main/java/top/spencer/crabscore/data/constant/Token.java
@@ -1,15 +1,13 @@
 package top.spencer.crabscore.data.constant;
 
+import top.spencer.crabscore.data.model.TestDataModel;
+
 /**
  * @author spencercjh
  */
 public class Token {
     /**
-     * 包名
-     */
-    private static final String PACKAGE_NAME = "top.spencer.crabscore.data.model.";
-    /**
      * 具体Model
      */
-    public static final String API_USER_DATA = PACKAGE_NAME + "TestDataModel";
+    public static final String API_USER_DATA = TestDataModel.class.getName();
 }
