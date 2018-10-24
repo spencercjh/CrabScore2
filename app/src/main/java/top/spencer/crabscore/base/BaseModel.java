4f32d1d2fc13ba992b0b3c61bd74aa2a436537b6
Spencercjh <shouspencercjh@foxmail.com>
2018-10-25 01:08:08 +0800

    complete BaseNetWork(OkHttp);

--- app/src/main/java/top/spencer/crabscore/base/BaseModel.java
+++ app/src/main/java/top/spencer/crabscore/base/BaseModel.java
@@ -1,12 +1,22 @@
 package top.spencer.crabscore.base;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import okhttp3.*;
+import top.spencer.crabscore.common.CommonConstant;
 import top.spencer.crabscore.data.Callback;
 
+import java.io.IOException;
 import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * @author spencercjh
  */
+@SuppressWarnings({"Duplicates", "unused"})
 public abstract class BaseModel<T> {
     /**
      * 数据请求参数
@@ -25,34 +35,215 @@ public abstract class BaseModel<T> {
 
     /**
      * 具体的数据请求由子类实现
+     *
+     * @param myCallBack myCallBack
      */
-    public abstract void execute(Callback<T> callback);
+    public abstract void execute(Callback<T> myCallBack);
 
     /**
-     * 执行Get网络请求，此类看需求由自己选择写与不写
+     * OkHttp3 异步Get请求
+     *
+     * @param url        URL (需要在外面处理好)
+     * @param myCallBack myCallBack
+     * @param jwt        Header里的JWT串
      */
-    protected void requestGetAPI(String url, Callback<T> callback) {
-        //TODO 这里写具体的网络请求
+    protected void requestGetAPI(String url, final Callback<Map<String, Object>> myCallBack, String jwt) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("jwt", jwt)
+                .build();
+        okHttpClient.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d(TAG, "onFailure: " + e.getMessage());
+                myCallBack.onError();
+                myCallBack.onComplete();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                String jwt = response.headers().get("jwt");
+                JSONObject responseJsonResult;
+                try {
+                    assert response.body() != null;
+                    Log.d(TAG, response.body().string());
+                    responseJsonResult = JSON.parseObject(response.body().string());
+                    responseJsonResult.put("jwt", jwt);
+                } catch (IOException | NullPointerException e) {
+                    e.printStackTrace();
+                    myCallBack.onError();
+                    myCallBack.onComplete();
+                    return;
+                }
+                Integer code = responseJsonResult.getInteger("code");
+                if (code.equals(CommonConstant.SUCCESS)) {
+                    myCallBack.onSuccess(responseJsonResult);
+                } else {
+                    myCallBack.onFailure(responseJsonResult.getString("message"));
+                }
+                myCallBack.onComplete();
+            }
+        });
     }
 
     /**
-     * 执行Post网络请求，此类看需求由自己选择写与不写
+     * OkHttp3 Post异步方式提交表单
+     *
+     * @param url        URL
+     * @param postParams body中的参数
+     * @param myCallBack myCallBack
+     * @param jwt        Header里的JWT串
      */
-    protected void requestPostAPI(String url, Map params, Callback<T> callback) {
-        //TODO 这里写具体的网络请求
+    protected void requestPostAPI(String url, Map<String, Object> postParams,
+                                  final Callback<Map<String, Object>> myCallBack, String jwt) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        FormBody.Builder formBody = new FormBody.Builder();
+        for (Map.Entry<String, Object> param : postParams.entrySet()) {
+            formBody.add(param.getKey(), param.getValue().toString());
+        }
+        RequestBody requestBody = formBody.build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .addHeader("jwt", jwt)
+                .build();
+        okHttpClient.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d(TAG, "onFailure: " + e.getMessage());
+                myCallBack.onError();
+                myCallBack.onComplete();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                String jwt = response.headers().get("jwt");
+                JSONObject responseJsonResult;
+                try {
+                    assert response.body() != null;
+                    Log.d(TAG, response.body().string());
+                    responseJsonResult = JSON.parseObject(response.body().string());
+                    responseJsonResult.put("jwt", jwt);
+                } catch (IOException | NullPointerException e) {
+                    e.printStackTrace();
+                    myCallBack.onError();
+                    myCallBack.onComplete();
+                    return;
+                }
+                Integer code = responseJsonResult.getInteger("code");
+                if (code.equals(CommonConstant.SUCCESS)) {
+                    myCallBack.onSuccess(responseJsonResult);
+                } else {
+                    myCallBack.onFailure(responseJsonResult.getString("message"));
+                }
+                myCallBack.onComplete();
+            }
+        });
     }
 
     /**
-     * 执行Post网络请求，此类看需求由自己选择写与不写
+     * OkHttp3 PUT异步请求
+     *
+     * @param url        URL
+     * @param putParams  body中的参数
+     * @param myCallBack myCallBack
+     * @param jwt        Header里的JWT串
      */
-    protected void requestPutAPI(String url, Map params, Callback<T> callback) {
-        //TODO 这里写具体的网络请求
+    protected void requestPutAPI(String url, Map<String, Object> putParams,
+                                 final Callback<Map<String, Object>> myCallBack, String jwt) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        FormBody.Builder formBody = new FormBody.Builder();
+        for (Map.Entry<String, Object> param : putParams.entrySet()) {
+            formBody.add(param.getKey(), param.getValue().toString());
+        }
+        RequestBody requestBody = formBody.build();
+        Request request = new Request.Builder()
+                .url(url)
+                .put(requestBody)
+                .addHeader("jwt", jwt)
+                .build();
+        okHttpClient.newCall(request).enqueue(new okhttp3.Callback() {
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d(TAG, "onFailure: " + e.getMessage());
+                myCallBack.onError();
+                myCallBack.onComplete();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                String jwt = response.headers().get("jwt");
+                JSONObject responseJsonResult;
+                try {
+                    assert response.body() != null;
+                    Log.d(TAG, response.body().string());
+                    responseJsonResult = JSON.parseObject(response.body().string());
+                    responseJsonResult.put("jwt", jwt);
+                } catch (IOException | NullPointerException e) {
+                    e.printStackTrace();
+                    myCallBack.onError();
+                    myCallBack.onComplete();
+                    return;
+                }
+                Integer code = responseJsonResult.getInteger("code");
+                if (code.equals(CommonConstant.SUCCESS)) {
+                    myCallBack.onSuccess(responseJsonResult);
+                } else {
+                    myCallBack.onFailure(responseJsonResult.getString("message"));
+                }
+                myCallBack.onComplete();
+            }
+        });
     }
 
     /**
-     * 执行Post网络请求，此类看需求由自己选择写与不写
+     * OkHttp3 Delete异步请求
+     *
+     * @param url        URL (需要在外面处理好)
+     * @param myCallBack myCallBack
+     * @param jwt        Header里的JWT串
      */
-    protected void requestDeleteAPI(String url, Map params, Callback<T> callback) {
-        //TODO 这里写具体的网络请求
+    protected void requestDeleteAPI(String url, final Callback<Map<String, Object>> myCallBack, String jwt) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .delete()
+                .addHeader("jwt", jwt)
+                .build();
+        okHttpClient.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d(TAG, "onFailure: " + e.getMessage());
+                myCallBack.onError();
+                myCallBack.onComplete();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                String jwt = response.headers().get("jwt");
+                JSONObject responseJsonResult;
+                try {
+                    assert response.body() != null;
+                    Log.d(TAG, response.body().string());
+                    responseJsonResult = JSON.parseObject(response.body().string());
+                    responseJsonResult.put("jwt", jwt);
+                } catch (IOException | NullPointerException e) {
+                    e.printStackTrace();
+                    myCallBack.onError();
+                    myCallBack.onComplete();
+                    return;
+                }
+                Integer code = responseJsonResult.getInteger("code");
+                if (code.equals(CommonConstant.SUCCESS)) {
+                    myCallBack.onSuccess(responseJsonResult);
+                } else {
+                    myCallBack.onFailure(responseJsonResult.getString("message"));
+                }
+                myCallBack.onComplete();
+            }
+        });
     }
 }
